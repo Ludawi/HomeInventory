a39ab5278cd4c45e44ee99f080aefbcf463ac60b
Ludawi <wistuba9@gmail.com>
2025-04-10 23:35:17 +0200

    local reader

--- main.py
+++ main.py
@@ -2,8 +2,21 @@ import cv2
 from pyzbar.pyzbar import decode
 
 
-img = cv2.imread('barcode.png')
+reader = cv2.VideoCapture(0)
+reader.set(3, 1280)  # 3 = Width
+reader.set(4, 720)  # 4 = Height
+camera = True
 
-for code in decode(img):
-    print(code.type)
-    print(code.data.decode('utf-8'))
+registered_codes = []
+
+while camera:
+    success,  frame = reader.read()
+
+    for code in decode(frame):
+        # print(code.type)
+        # print(code.data.decode('utf-8'))
+        registered_codes.append(code.data.decode('utf-8'))
+        print(registered_codes)
+
+    cv2.imshow('code-scan', frame)
+    cv2.waitKey(2)
