61a48ccdc187c73d88ee5ec9277b9519299fd46f
Ludawi <wistuba9@gmail.com>
2025-04-12 01:21:49 +0200

    refactored db functions to Db class

---
+++ db.py
@@ -0,0 +1,131 @@
+import sqlite3
+from datetime import datetime
+from flask import g
+
+DATABASE = './db/database.db'
+
+
+class Db:
+    @staticmethod
+    def get_connection():
+        db = getattr(g, '_database', None)
+        if db is None:
+            db = g._database = sqlite3.connect(DATABASE)
+        return db
+
+    @staticmethod
+    def close_connection(exception):
+        db = getattr(g, '_database', None)
+        if db is not None:
+            db.close()
+
+    @staticmethod
+    def list_items():
+        db = Db.get_connection()
+        cur = db.cursor()
+        cur.execute(
+            'SELECT id, code_type, code, description, item_count, registered_at FROM items')
+        rows = cur.fetchall()
+        cur.close()
+
+        return [
+            {
+                'id': row[0],
+                'code_type': row[1],
+                'code': row[2],
+                'description': row[3],
+                'count': row[4],
+                'registered_at': row[5]
+            } for row in rows
+        ]
+
+        # result = [
+        #     {
+        #         'id': row[0],
+        #         'code_type': row[1],
+        #         'code': row[2],
+        #         'description': row[3],
+        #         'count': row[4],
+        #         'registered_at': row[5]
+        #     }
+        #     for row in rows
+        # ]
+        #
+        # return jsonify(result)
+
+    @staticmethod
+    def item_exists(code):
+        db = Db.get_connection()
+        cur = db.cursor()
+        cur.execute(
+            'SELECT item_count FROM items WHERE code=? LIMIT 1', (code,))
+        result = cur.fetchone()
+        cur.close()
+        return result[0] if result else None
+
+    @staticmethod
+    def add_count(code, amount, timestamp):
+
+        count = amount + 1
+        db = Db.get_connection()
+        cur = db.cursor()
+        cur.execute('''
+            UPDATE items
+            SET item_count=?, timestamp=?
+            WHERE code=?
+        ''', (count, timestamp, code,))
+
+        db.commit()
+        cur.close()
+        return 200
+
+    @staticmethod
+    def substract_count(code, amount, timestamp):
+        count = amount - 1
+        db = Db.get_connection()
+        cur = db.cursor()
+        cur.execute('''
+            UPDATE items
+            SET item_count=?, timestamp=?
+            WHERE code=?
+        ''', (count, timestamp, code))
+        db.commit()
+        cur.close()
+        return 200
+
+    @staticmethod
+    def add_item(item):
+        code_type = item.get("type")
+        code = item.get("data")
+        timestamp_iso = datetime.strptime(
+            item.get("timestamp"), "%Y-%m-%dT%H:%M:%S.%fZ")
+        timestamp = timestamp_iso.strftime("%Y-%m-%d %H:%M:%S.%f")[:-3]
+
+        amount = Db.item_exists(code)
+        db = Db.get_connection()
+        cur = db.cursor()
+
+        if amount is None:
+            cur.execute('''
+                INSERT INTO items (code_type, code, timestamp, item_count)
+                VALUES (?, ?, ?, ?)
+            ''', (code_type, code, timestamp, 1))
+            db.commit()
+            cur.close()
+            return 201
+        else:
+            cur.close()
+            return Db.add_count(code, amount, timestamp)
+
+    @staticmethod
+    def remove_item(item):
+        code = item.get("data")
+        timestamp_iso = datetime.strptime(
+            item.get("timestamp"), "%Y-%m-%dT%H:%M:%S.%fZ")
+        timestamp = timestamp_iso.strftime("%Y-%m-%d %H:%M:%S.%f")[:-3]
+        amount = Db.item_exists(code)
+
+        if amount is None or amount <= 0:
+            return 304
+        else:
+            return Db.substract_count(code, amount, timestamp)
--- server.py
+++ server.py
@@ -1,25 +1,12 @@
 from flask import Flask, request, Response, jsonify, g, render_template
-import sqlite3
-from datetime import datetime
-
+from db import Db
 
 app = Flask(__name__)
-
-DATABASE = './db/database.db'
-
-
-def get_db():
-    db = getattr(g, '_database', None)
-    if db is None:
-        db = g._database = sqlite3.connect(DATABASE)
-    return db
 
 
 @app.teardown_appcontext
 def close_connection(exception):
-    db = getattr(g, '_database', None)
-    if db is not None:
-        db.close()
+    Db.close_connection(exception)
 
 
 @app.route('/')
@@ -47,91 +34,7 @@ def health():
 
 @app.route('/db')
 def db_list():
-    db = get_db()
-    cur = db.cursor()
-    cur.execute(
-        'SELECT id, code_type, code, description, item_count, registered_at FROM items')
-    rows = cur.fetchall()
-    cur.close()
-
-    result = [
-        {
-            'id': row[0],
-            'code_type': row[1],
-            'code': row[2],
-            'description': row[3],
-            'count': row[4],
-            'registered_at': row[5]
-        }
-        for row in rows
-    ]
-
-    return jsonify(result)
-
-
-def item_exists(code):
-    db = get_db()
-    cur = db.cursor()
-
-    cur.execute('SELECT 1 FROM items WHERE code=? LIMIT 1', (code,))
-    result = cur.fetchone()
-    print("DB result:", result)
-    if result is None:
-        db.commit()
-        cur.close()
-        return False
-    else:
-        cur.execute(
-            'SELECT item_count FROM items WHERE code=? LIMIT 1', (code,))
-        result = cur.fetchone()
-
-    if result is None:
-        db.commit()
-        cur.close()
-        return False
-    else:
-        return result[0]
-
-
-def add_count(code, amount, timestamp):
-
-    count = amount + 1
-    db = get_db()
-    cur = db.cursor()
-    cur.execute('''
-        UPDATE items
-        SET item_count=?, timestamp=?
-        WHERE code=?
-    ''', (count, timestamp, code,))
-
-    db.commit()
-    cur.close()
-    return 200
-
-
-def add_item(item):
-
-    code_type = item.get("type")
-    code = item.get("data")
-    timestamp_iso = datetime.strptime(
-        item.get("timestamp"), "%Y-%m-%dT%H:%M:%S.%fZ")
-    timestamp = timestamp_iso.strftime("%Y-%m-%d %H:%M:%S.%f")[:-3]
-    amount = item_exists(code)
-    if amount == None:
-        # Create new entry
-        db = get_db()
-        cur = db.cursor()
-        cur.execute('''
-            INSERT INTO items (code_type, code, timestamp, item_count)
-            VALUES (?, ?, ?, ?)
-        ''', (code_type, code, timestamp, 1))
-
-        db.commit()
-        cur.close()
-        return 201
-    else:
-        add_count(code, amount, timestamp)
-        return 200
+    return jsonify(Db.list_items())
 
 
 @app.route('/register', methods=['POST'])
@@ -140,48 +43,17 @@ def register():
         return jsonify({"error": "Request must be JSON"}), 400
 
     data = request.get_json()
-
     required_fields = {"timestamp", "type", "data"}
+
     if not required_fields.issubset(data):
         return jsonify({"error": "Missing required fields"}), 400
 
-    return_code = add_item(data)
+    return_code = Db.add_item(data)
 
     return jsonify({
         "message": "Registration successful",
         "received": data
     }), return_code
-
-
-def substract_count(code, amount, timestamp):
-
-    count = amount - 1
-    db = get_db()
-    cur = db.cursor()
-    cur.execute('''
-        UPDATE items
-        SET item_count=?, timestamp=?
-        WHERE code=?
-    ''', (count, timestamp, code,))
-
-    db.commit()
-    cur.close()
-    return 200
-
-
-def remove_item(item):
-
-    code = item.get("data")
-    timestamp_iso = datetime.strptime(
-        item.get("timestamp"), "%Y-%m-%dT%H:%M:%S.%fZ")
-    timestamp = timestamp_iso.strftime("%Y-%m-%d %H:%M:%S.%f")[:-3]
-    amount = item_exists(code)
-
-    if amount == None or amount <= 0:
-        return 304
-    else:
-        substract_count(code, amount, timestamp)
-        return 200
 
 
 @app.route('/unregister', methods=['POST'])
@@ -190,13 +62,12 @@ def unregister():
         return jsonify({"error": "Request must be JSON"}), 400
 
     data = request.get_json()
-
-    # Validation
     required_fields = {"timestamp", "type", "data"}
+
     if not required_fields.issubset(data):
         return jsonify({"error": "Missing required fields"}), 400
 
-    return_code = remove_item(data)
+    return_code = Db.remove_item(data)
 
     return jsonify({
         "message": "Unregistration successful",
