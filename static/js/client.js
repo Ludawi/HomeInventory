bc8d23f94632c35e8dee2ff1f7991aaa616f5b32
Ludawi <wistuba9@gmail.com>
2025-04-14 03:50:18 +0200

    added web client

--- static/js/client.js
+++ static/js/client.js
@@ -1,3 +1,19 @@
 document.addEventListener('DOMContentLoaded', function () {
     console.log("JavaScript is working!");
 });
+fetch('/api/record', {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json'  // sending JSON
+    },
+    body: JSON.stringify({
+        code: '123',
+    })
+})
+.then(response => response.json())
+.then(data => {
+    console.log('Server response:', data);
+})
+.catch(error => {
+    console.error('Error:', error);
+});
